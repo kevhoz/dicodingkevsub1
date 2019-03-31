3b798881a114d3a07bf3e43f663db75f390b71d1
Kevin Christianto <kevin.hikoza@gmail.com>
2019-03-31 21:23:45 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,25 @@
+<html>
+<head>
+<title>REGISTER HERE !!!!</title>
+<h3>Fill in your name, address and job, then click <b>Submit</b> to register</h3>
+</head>
+<body>
+<form action="submit.php" name="frmAdd" method="post">
+<table width="284" border="1">
+  <tr>
+    <th width="120">Name</th>
+    <td><input type="text" name="txtName" size="20"></td>
+    </tr>
+  <tr>
+    <th width="120">Email</th>
+    <td><input type="text" name="txtEmail" size="20"></td>
+    </tr>
+  <tr>
+    <th width="120">Job</th>
+    <td><input type="text" name="txtJob" size="20"></td>
+    </tr>
+  </table>
+  <input type="submit" name="submit" value="submit">
+</form>
+</body>
+</html>
