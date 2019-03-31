34777993f6491618e28ec29a98e405f1331d0e63
Kevin Christianto <kevin.hikoza@gmail.com>
2019-03-31 21:42:10 +0700

    submit.php

---
+++ submit.php
@@ -0,0 +1,38 @@
+<html>
+<head>
+<title>Submitted</title>
+</head>
+<body>
+<?php
+	ini_set('display_errors', 1);
+	error_reporting(~0);
+
+	$serverName = "dicodingkevsub1.database.windows.net";
+	$userName = "kevhoz";
+	$userPassword = 'Zxcasdqwe123';
+	$dbName = "kevsub1";
+
+	$connectionInfo = array("Database"=>$dbName, "UID"=>$userName, "PWD"=>$userPassword, "MultipleActiveResultSets"=>true);
+
+	$conn = sqlsrv_connect( $serverName, $connectionInfo);
+
+	if( $conn === false ) {
+		die( print_r( sqlsrv_errors(), true));
+	}
+
+	$sql = "INSERT INTO customer (Name, Email, Job) VALUES (?, ?, ?)";
+	$params = array($_POST["txtName"], $_POST["txtEmail"], $_POST["txtJob"]);
+
+	$stmt = sqlsrv_query( $conn, $sql, $params);
+	if( $stmt === false ) {
+		 die( print_r( sqlsrv_errors(), true));
+	}
+	else
+	{
+		echo "Record add successfully";
+	}
+
+	sqlsrv_close($conn);
+?>
+</body>
+</html>
